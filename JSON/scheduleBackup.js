6b1aec70795d8a36085d31738bffecc58ace55a7
Fuglen <simon0210@hotmail.dk>
2019-12-16 20:59:46 +0100

    just a backup

---
+++ JSON/scheduleBackup.js
@@ -0,0 +1,76 @@
+var schedule = {};
+
+schedule =
+{
+    "team": "",
+    "class": "3",
+    "schedule": [
+        {
+            "mandag": [
+                {
+                    "1": "ENGELSK",
+                    "2": "DANSK",
+                    "3": "HISTORIE",
+                    "4": "HISTORIE",
+                    "5": "KRISTENDOM",
+                    "6": "",
+                    "7": ""
+                }
+            ]
+        },
+        {
+            "tirsdag": [
+                {
+                    "1": "BILLEDKUNST",
+                    "2": "BILLEDKUNST",
+                    "3": "BILLEDKUNST",
+                    "4": "MUSIK",
+                    "5": "MATEMATIK",
+                    "6": "MATEMATIK",
+                    "7": "LEKTIECAFE"
+                }
+            ]
+        },
+        {
+            "onsdag": [
+                {
+                    "1": "NATUR/TEKNIK",
+                    "2": "NATUR/TEKNIK",
+                    "3": "IDRÆT",
+                    "4": "IDRÆT",
+                    "5": "DANSK",
+                    "6": "KRISTENDOM",
+                    "7": ""
+                }
+            ]
+        },
+        {
+            "torsdag": [
+                {
+                    "1": "MATEMATIK",
+                    "2": "MATEMATIK",
+                    "3": "HISTORIE",
+                    "4": "ENGELSK",
+                    "5": "DANSK",
+                    "6": "DANSK",
+                    "7": "LEKTIECAFE"
+                }
+            ]
+        },
+        {
+            "fredag": [
+                {
+                    "1": "HISTORIE",
+                    "2": "HISTORIE",
+                    "3": "ENGELSK",
+                    "4": "ENGELSK",
+                    "5": "",
+                    "6": "",
+                    "7": ""
+                }
+            ]
+        }
+    ]
+};
+
+module.exports = schedule;
